b62c131a52170e546ecc28399d7a093a206e21bf
Yohannes Takata <yohannestakata1@gmail.com>
2024-05-14 01:58:23 +0300

    Add project files.

---
+++ Factorial Checker/Form1.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        TextBox2 = New TextBox()
+        Button1 = New Button()
+        Label3 = New Label()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Font = New Font("Segoe UI", 16F)
+        TextBox1.Location = New Point(12, 102)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(378, 50)
+        TextBox1.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(12, 74)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(121, 25)
+        Label1.TabIndex = 1
+        Label1.Text = "Input number"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(396, 74)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(77, 25)
+        Label2.TabIndex = 2
+        Label2.Text = "Factorial"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Font = New Font("Segoe UI", 48F)
+        TextBox2.Location = New Point(396, 102)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(392, 135)
+        TextBox2.TabIndex = 3
+        TextBox2.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(278, 203)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(112, 34)
+        Button1.TabIndex = 4
+        Button1.Text = "Calculate"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 16F)
+        Label3.Location = New Point(256, 9)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(290, 45)
+        Label3.TabIndex = 5
+        Label3.Text = "Factorial Calculator"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label3)
+        Controls.Add(Button1)
+        Controls.Add(TextBox2)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
+
+End Class
---
+++ Factorial Checker/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim input As String = TextBox1.Text
+        Dim result As String
+
+        If IsNumeric(input) Then
+            Dim number As Integer = CInt(input)
+            If number < 0 Then
+                result = "Factorial is not defined by negatives."
+            Else
+                result = CalculateFactorial(number).ToString()
+            End If
+
+            TextBox2.Text = result
+        End If
+    End Sub
+
+    Private Function CalculateFactorial(number As Integer) As Long
+        Dim factorial As Long = 1
+        For i As Integer = 1 To number
+            factorial *= i
+        Next
+        Return factorial
+    End Function
+End Class
